e662b278aa182e255cc7d9aa00fa8d9f811843cd
Stanislav Verman <verman19837@gmail.com>
2022-11-07 15:50:46 +0700

    create app frame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import Header from "./components/Header/Header";
+import Categories from "./components/Categories/Categories";
+import SortPopup from "./components/SortPopup/SortPopup";
+import PizzaBlock from "./components/PizzBlock/PizzaBlock";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
+                        <SortPopup />
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                    <PizzaBlock />
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
